30b10fd87efd4dee6acd3537d5df52b8dc494240
dangtruong06 <DANGTT066@GMAIL.COM>
2026-08-02 12:06:31 -0700

    set up user and expense models, flask-migrate and created db tables. Set up auth with JWT next

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify
 from extensions import db
 from config import Config
+from flask_migrate import Migrate
+from models import User, Expense
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
+migrate = Migrate(app,db)
 
 @app.route('/')
 def index(): 
--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,31 @@
-from flask import SQLAlchemy
+from extensions import db 
+from sqlalchemy import String, Float, ForeignKey, Date, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import date, datetime
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    email:Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    password_hash:Mapped[str] = mapped_column(String(250), nullable=False)
+
+    #relationship
+    expenses:Mapped[list["Expense"]] = relationship(
+        back_populates='user', cascade="all, delete-orphan"
+    )
+
+class Expense(db.Model):
+    __tablename__ = 'expenses'
+
+    id:Mapped[int] = mapped_column(primary_key=True)
+    name:Mapped[str] = mapped_column(String(250), nullable=False)
+    amount:Mapped[float] = mapped_column(Float, nullable=False)
+    category:Mapped[str] = mapped_column(String(250), nullable=False)
+    expense_date:Mapped[date] = mapped_column(Date, nullable=False)
+    created_at:Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    
+
+    #relationship
+    user_id:Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user:Mapped["User"] = relationship(back_populates='expenses')
