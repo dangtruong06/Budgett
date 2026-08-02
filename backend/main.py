bdde868ddb93df0d78439bada97b92c457d186bd
dangtruong06 <DANGTT066@GMAIL.COM>
2026-08-02 11:25:31 -0700

    set up db connection with flask-sqlalchemy

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify
+from extensions import db
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 
 @app.route('/')
 def index(): 
-    return jsonify({'message': 'Budgett API running!'})
+    return jsonify({'message': 'Budgett API running meow!'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
