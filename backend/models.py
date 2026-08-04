55bd853f8e3a35e2e72af971cd97181a128d1c75
dangtruong06 <DANGTT066@GMAIL.COM>
2026-08-04 16:54:05 -0700

    add expense CRUD routes with JWT ownership checks

--- backend/models.py
+++ backend/models.py
@@ -24,8 +24,19 @@ class Expense(db.Model):
     category:Mapped[str] = mapped_column(String(250), nullable=False)
     expense_date:Mapped[date] = mapped_column(Date, nullable=False)
     created_at:Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
-    
 
     #relationship
     user_id:Mapped[int] = mapped_column(ForeignKey("users.id"))
     user:Mapped["User"] = relationship(back_populates='expenses')
+
+    # method to return as json
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'amount': self.amount,
+            'category': self.category,
+            'expense_date': self.expense_date.isoformat(),
+            'created_at': self.created_at.isoformat(), 
+            'user_id': self.user_id
+        }
